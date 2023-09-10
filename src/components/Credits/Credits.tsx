c1443c10ab9599c7195f1f70eb6730e40afad1c9
krixfega <krixfega@gmail.com>
2023-09-10 21:01:05 +0100

    the entire file
    updated

---
+++ src/components/Credits/Credits.tsx
@@ -0,0 +1,17 @@
+function Credits() {
+  return (
+    <div>
+      <small>
+        Remade by <a href="https://chrisfega.netlify.com">Chris Fega</a>
+        <br />
+        View{' '}
+        {/* <a href="https://github.com/krixfega/whatsapp-exported-chat-reader">
+          Source code
+        </a> */}
+      </small>
+      <small> First made by Loris Bettaza</small>
+    </div>
+  );
+}
+
+export default Credits;
